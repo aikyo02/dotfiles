5c52bd0518361e89ce3d76228a2999e2af61c260
aikyo <aikyo02@gmail.com>
2015-09-29 06:33:58 +0900

    fix installation script.

--- install.sh
+++ install.sh
@@ -2,11 +2,10 @@
 
 curl -L "https://github.com/aikyo02/dotfiles/archive/master.tar.gz" | tar xv
 mv dotfiles-master ~/dotfiles
-cd ~/dotfiles
 
-for f in .??*
+for f in ~/dotfiles/.??*
 do
     [ "$f" = ".git" ] && continue
 
-    ln -snfv "$f" "$HOME"/"$f"
+    ln -snfv ~/dotfiles/"$f" "$HOME"/"$f"
 done
